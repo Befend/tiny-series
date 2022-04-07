b906714c056e80b23fa352543caf61e38cae76b6
Befend <18814382464@163.com>
2022-04-07 21:24:24 +0800

    实现effect & reactive & 依赖收集 & 触发依赖

---
+++ tiny-vue3/src/reactivity/effect.ts
@@ -0,0 +1,46 @@
+
+class ReactiveEffect {
+  private _fn: any
+
+  constructor(fn) {
+    this._fn = fn
+  }
+
+  run() {
+    activeEffect = this
+    this._fn()
+  }
+}
+
+const targetMap = new Map()
+export function track(target, key) {
+  // target -> key -> dep
+  let desMap = targetMap.get(target)
+  if (!desMap) {
+    desMap = new Map()
+    targetMap.set(target, desMap)
+  }
+
+  let dep = desMap.get(key)
+  if(!dep) {
+    dep = new Set();
+    desMap.set(key, dep)
+  }
+  dep.add(activeEffect)
+}
+
+export function trigger(target, key) {
+  let desMap = targetMap.get(target)
+  let dep = desMap.get(key)
+  for(const effect of dep) {
+    effect.run()
+  }
+}
+
+let activeEffect
+export function effect(fn, options:any = {}) {
+  // fn
+  const _effect = new ReactiveEffect(fn)
+
+  _effect.run()
+} 
