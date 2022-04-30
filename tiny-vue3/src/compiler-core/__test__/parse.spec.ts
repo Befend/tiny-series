bc977ff32ee57d89bee434da1f4a4e0ca6697b0e
Befend <18814382464@163.com>
2022-04-30 20:52:47 +0800

    实现解析 element

--- tiny-vue3/src/compiler-core/__test__/parse.spec.ts
+++ tiny-vue3/src/compiler-core/__test__/parse.spec.ts
@@ -1,11 +1,10 @@
 import { baseParse } from "../src/parse"
 import { NodeTypes } from "../src/ast"
 
-describe('Parse', () => {
-  describe('interpolation', () => {
-    test('simple interpolation', () => {
+describe("Parse", () => {
+  describe("interpolation", () => {
+    test("simple interpolation", () => {
       const ast = baseParse("{{message}}")
-
       // root
       expect(ast.children[0]).toStrictEqual({
         type: NodeTypes.INTERPOLATION,
@@ -13,6 +12,18 @@ describe('Parse', () => {
           type: NodeTypes.SIMPLE_EXPRESSION,
           content: "message"
         }
+      })
+    })
+  })
+
+  describe("element", () => {
+    it("simple element div", () => {
+      const ast = baseParse("<div></div>");
+
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.ELEMENT,
+        tag: "div",
+        children: [],
       })
     })
   })
--- tiny-vue3/src/compiler-core/src/parse.ts
+++ tiny-vue3/src/compiler-core/src/parse.ts
@@ -1,5 +1,9 @@
 import { NodeTypes } from "./ast"
 
+const enum TagType {
+  Start,
+  End
+}
 export function baseParse(content: string) {
   const context = createParseContext(content)
   return createRoot(parseChildren(context))
@@ -8,19 +12,55 @@ export function baseParse(content: string) {
 function parseChildren(context) {
   const nodes: any = []
   let node
-  if (context.source.startsWith("{{")) {
+  const s = context.source
+  if (s.startsWith("{{")) {
     node = parseInterpolation(context)
+  } else if (s[0] === "<") {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context)
+    }
   }
   nodes.push(node)
   return nodes
+}
+
+function parseElement(context) {
+  // Implement
+  // 1. 解析 tag
+  const element = parseTag(context, TagType.Start)
+  parseTag(context, TagType.End)
+  console.log("————————————", context.source);
+
+  return element
+}
+
+function parseTag(context: any, type: TagType) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source)
+  const tag = match[1]
+  // 2. 删除处理完成的代码
+  advanceBy(context, match[0].length)
+  advanceBy(context, 1)
+
+  if (type === TagType.End) return
+
+  return {
+    type: NodeTypes.ELEMENT,
+    children: [],
+    tag
+  }
 }
 
 function parseInterpolation(context) {
   // {{message}}
   const openDelimiter = "{{"
   const closeDelimiter = "}}"
-  const closeIndex = context.source.indexOf(closeDelimiter, openDelimiter.length)
+
+  const closeIndex = context.source.indexOf(
+    closeDelimiter,
+    openDelimiter.length
+  )
   advanceBy(context, openDelimiter.length)
+
   const rawContentLength = closeIndex - openDelimiter.length
   const rawContent = context.source.slice(0, rawContentLength)
   const content = rawContent.trim()
